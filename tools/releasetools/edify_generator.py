1e39ace23dfee46b35bc39fab8ca6ca433c0f83c
IU <alexfinhart@gmail.com>
2021-09-24 15:24:12 +0800

    initialize ArcaneOS

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -268,11 +268,11 @@ class EdifyGenerator(object):
                                   security_patch, device):
     self.Print("----------------------------------------------")
     if is_plus:
-      self.Print("        PixelExperience (Plus edition)")
-      self.Print("                by jhenrique09")
+      self.Print("              ArcaneOS")
+      self.Print("                by dlwlrma123")
     else:
-      self.Print("              PixelExperience")
-      self.Print("              by jhenrique09")
+      self.Print("              ArcaneOS")
+      self.Print("              by dlwlrma123")
     self.Print("----------------------------------------------")
     self.Print(" Android version: %s"%(android_version))
     self.Print(" Build id: %s"%(build_id))
